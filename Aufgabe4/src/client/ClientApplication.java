99dbf793fab18edffd52c6377c4813f884e26876
Anton K <s0553324@htw-berlin.de>
2017-06-25 12:13:59 +0200

    Aufgabe4

--- Aufgabe4/src/client/ClientApplication.java
+++ Aufgabe4/src/client/ClientApplication.java
@@ -9,7 +9,6 @@ public class ClientApplication {
      *
      * @param args - not used
      */
-
     public static void main(String[] args) {
         new Client().run();
     }
--- Aufgabe4/src/server/Server.java
+++ Aufgabe4/src/server/Server.java
@@ -4,6 +4,7 @@ import protocol.WeatherDataProtocol;
 import util.DateCSVReader;
 
 import java.io.*;
+import java.lang.management.ManagementFactory;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Path;
@@ -11,10 +12,18 @@ import java.nio.file.Paths;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Server implements Runnable, AutoCloseable {
 
     private final ServerSocket host;
+    private ExecutorService threadPool;
+    private Logger logger;
     /**
      * Value format: Min,Max,Average,24h values
      */
@@ -24,8 +33,17 @@ public class Server implements Runnable, AutoCloseable {
         host = new ServerSocket(WeatherDataProtocol.SERVER_PORT);
         final Path path = Paths.get("resources/data.csv");
         localDateToTimesMap = new DateCSVReader().readFile(path);
+        threadPool=Executors.newCachedThreadPool();
+
+        initLogger();
         computeMinMaxAverage();
-        System.out.println(localDateToTimesMap);
+    }
+    private void initLogger() throws IOException{
+        logger=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+        FileHandler fh= new FileHandler("resources/log.txt");
+        fh.setFormatter(new SimpleFormatter());
+        logger.addHandler(fh);
     }
 
     private void computeMinMaxAverage() {
@@ -37,8 +55,8 @@ public class Server implements Runnable, AutoCloseable {
             int sumTemperature = 0;
             int minTemperature = maxTemperature = Integer.valueOf(data[0]);
 
-            for (int i = 0; i < data.length; i++) {
-                int temperatureData = Integer.valueOf(data[i]);
+            for (String temperature : data) {
+                int temperatureData = Integer.valueOf(temperature);
                 sumTemperature += temperatureData;
                 if (temperatureData < minTemperature)
                     minTemperature = temperatureData;
@@ -56,10 +74,8 @@ public class Server implements Runnable, AutoCloseable {
         while (true) {
             try {
                 //block and wait for an incoming connection
-                Socket s = host.accept();
-                //no other requests can be accepted while the current one is processed.
-                //Shouldn't be a problem since processing is quick.
-                handleRequest(s);
+                Socket clientConnection = host.accept();
+                threadPool.submit(new ConnectionHandler(clientConnection));
             } catch (IOException e) {
                 //if something goes wrong just stop
                 e.printStackTrace();
@@ -68,35 +84,59 @@ public class Server implements Runnable, AutoCloseable {
         }
     }
 
-
-    private void handleRequest(Socket s) throws IOException {
-        try (Socket socket = s) {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                String request = reader.readLine();
-                String response;
-                try {
-                    //format it
-                    LocalDate date = WeatherDataProtocol.processRequest(request);
-                    //check if we an entry for the date
-                    response = localDateToTimesMap.get(date);
-                    if (response == null)
-                        response = WeatherDataProtocol.ERROR_NO_INFO_FOR_DATE;
-                } catch (DateTimeException e) {
-                    response = WeatherDataProtocol.ERROR_DATE_PARSE;
-                }
-                //send it to the client
-                try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-                    writer.write(response);
-                }
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
+    @Override
+    public void close() throws IOException {
+        try {
+            this.host.close();
+        }finally {
+            threadPool.shutdown();
         }
     }
 
+    private class ConnectionHandler implements Runnable {
+        Socket clientConnection;
 
-    @Override
-    public void close() throws IOException {
-        this.host.close();
+        public ConnectionHandler(Socket clientConnection) {
+            this.clientConnection = clientConnection;
+        }
+
+        @Override
+        public void run() {
+            //PID-should be the same for all threads, they are all part of the same process
+            String pid=ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+            //java thread id
+            long threadID=Thread.currentThread().getId();
+            String message= String.format("Address: %s:%d PID: %s ThreadID: %d",clientConnection.getInetAddress(),clientConnection.getPort(),pid,threadID);
+            logger.log(Level.INFO,message);
+
+            try (Socket clientConnection = this.clientConnection) {
+                System.out.println("Processing");
+                //delay for demonstration purposes
+                Thread.sleep(5000);
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()))) {
+                    String request = reader.readLine();
+                    String response;
+                    try {
+                        //format it
+                        LocalDate date = WeatherDataProtocol.processRequest(request);
+                        //check if we have an entry for the date
+                        response = localDateToTimesMap.get(date);
+                        if (response == null)
+                            response = WeatherDataProtocol.ERROR_NO_INFO_FOR_DATE;
+                    } catch (DateTimeException e) {
+                        response = WeatherDataProtocol.ERROR_DATE_PARSE;
+                    }
+                    //send it to the client
+                    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientConnection.getOutputStream()))) {
+                        writer.write(response);
+                    }
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+            System.out.println("Done");
+        }
+
     }
 }
